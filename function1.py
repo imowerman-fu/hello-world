4096a0b40412f6f257c6bc11f8e436d57019d8a3
imowerman-fu <imowerman@fairfield.edu>
2026-01-28 15:30:43 -0500

    functions

---
+++ function1.py
@@ -0,0 +1,16 @@
+# this program prompts the user 
+# for their name and greets them
+
+def main():
+    # output greeting
+    name = input("what's your name? ")
+    hello(name)
+
+    # Output without passing 
+    # the expected argument
+    hello()
+
+def hello(to='John Doe'):
+    print("Hello," , to)
+    
+main()
